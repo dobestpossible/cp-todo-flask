cf9a02bcc3f0273e5595d7ca94d22494aa36c06d
Glenn Gawron <dobestpossible@gmail.com>
2021-02-15 18:16:57 -0800

    Added features
     Connected database to the front-end form for adding new todo Items.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,14 +6,27 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(200))
     complete = db.Column(db.Boolean)
 
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    incomplete = Todo.query.filter_by(complete=False).all()
+
+    return render_template('index.html', incomplete=incomplete)
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    todo = Todo(text=request.form['todoItem'], complete=False)
+    db.session.add(todo)
+    db.session.commit()
+
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
